6c564c0fdb66c8b80963c0438f722ad7a4307317
ahahah <ahahah@DaeHwans-MacBook-Pro.local>
2025-05-23 16:16:07 +0900

    temp branch 작업 완료

--- app/src/main/java/com/ahin/knockpass/SensorActivity.kt
+++ app/src/main/java/com/ahin/knockpass/SensorActivity.kt
@@ -1,19 +1,29 @@
 package com.ahin.knockpass
 
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+
 
 class SensorActivity : AppCompatActivity(), SensorEventListener {
 
     private lateinit var sensorManager: SensorManager
     private var accelerometer: Sensor? = null
     private var gyroscope: Sensor? = null
-
+    private lateinit var audio: MicRecorder  // 녹음 클래스 (이전 MicRecorder)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_sensor)
 
         sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
         accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
         gyroscope = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE)
+        audio = MicRecorder()  // 오디오 초기화
     }
 
     override fun onResume() {
@@ -24,11 +34,13 @@ class SensorActivity : AppCompatActivity(), SensorEventListener {
         gyroscope?.also {
             sensorManager.registerListener(this, it, SensorManager.SENSOR_DELAY_NORMAL)
         }
+        audio.startRecording()
     }
 
     override fun onPause() {
         super.onPause()
         sensorManager.unregisterListener(this)
+        audio.stopRecording()
     }
 
     override fun onSensorChanged(event: SensorEvent?) {
