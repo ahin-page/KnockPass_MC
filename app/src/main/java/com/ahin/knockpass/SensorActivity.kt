5bd8d31d5d8c724785af4c9238c490e4af60916e
ahahah <ahahah@DaeHwans-MacBook-Pro.local>
2025-05-23 15:48:16 +0900

    c

--- app/src/main/java/com/ahin/knockpass/SensorActivity.kt
+++ app/src/main/java/com/ahin/knockpass/SensorActivity.kt
@@ -9,35 +9,40 @@ import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 
-
 class SensorActivity : AppCompatActivity(), SensorEventListener {
 
     private lateinit var sensorManager: SensorManager
     private var accelerometer: Sensor? = null
     private var gyroscope: Sensor? = null
 
+    private lateinit var audio: MicRecorder  // 녹음 클래스 (이전 MicRecorder)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sensor)
 
-        sensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
         accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
         gyroscope = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE)
+
+        audio = MicRecorder()  // 오디오 초기화
     }
 
     override fun onResume() {
         super.onResume()
         accelerometer?.also {
-            sensorManager.registerListener(this, it, SensorManager.SENSOR_DELAY_NORMAL)
+            sensorManager.registerListener(this, it, SensorManager.SENSOR_DELAY_FASTEST)
         }
         gyroscope?.also {
-            sensorManager.registerListener(this, it, SensorManager.SENSOR_DELAY_NORMAL)
+            sensorManager.registerListener(this, it, SensorManager.SENSOR_DELAY_FASTEST)
         }
+        audio.startRecording()
     }
 
     override fun onPause() {
         super.onPause()
         sensorManager.unregisterListener(this)
+        audio.stopRecording()
     }
 
     override fun onSensorChanged(event: SensorEvent?) {
@@ -55,8 +60,6 @@ class SensorActivity : AppCompatActivity(), SensorEventListener {
                     val gz = event.values[2]
                     Log.d("Gyroscope", "X:$gx, Y:$gy, Z:$gz")
                 }
-
-                else -> {}
             }
         }
     }
